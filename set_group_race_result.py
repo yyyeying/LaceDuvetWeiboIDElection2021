e79c9209705dbf453ac69f91b953e02149225477
yyyeying <henrybai678121@163.com>
2021-11-02 22:21:02 +0800

    更新第二天比赛结果

--- set_group_race_result.py
+++ set_group_race_result.py
@@ -1,10 +1,11 @@
 from candidate_utils import set_race_result_by_group, get_candidate_info_by_group
 
 if __name__ == '__main__':
-    group = 'E'
-    result = {'蕾丝傻狍子d酱': [34, True],
-              '蕾丝馒头d酱': [9, True],
-              '蕾丝豆汁d酱': [7, False],
-              '蕾丝包包d酱': [6, False],
-              '蕾包丝子d酱': [2, False]}
+    group = 'F'
+    result = {'蕾丝狍子d酱': [13, True],
+              '蕾丝肘子d酱': [12, True],
+              'd丝酱包子雷': [8, False],
+              '蕾丝糯米鸡d酱': [8, False],
+              '蕾丝馄饨d酱': [6, False],
+              '蕾丝馅儿饼d酱': [1, False]}
     set_race_result_by_group(group, result)
